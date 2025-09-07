ea035baecec2942f27cddc867551b1a8331a00a5
gromx-log <filbertferdinand270@gmail.com>
2025-09-07 20:07:51 +0700

    add function to count molal, molar, and pH;
    add mr (massa relatif) as new parameters

--- Tugas_1/index.js
+++ Tugas_1/index.js
@@ -8,13 +8,15 @@ const containerKaKb = document.getElementById("container-ka-kb");
 const containerMolality = document.getElementById("container-molality");
 const containerMass = document.getElementById("container-mass");
 const containerVolume = document.getElementById("container-volume");
+const containerMr = document.getElementById("container-mr");
 
 //Menyimpan nilai dari setiap form (baik section pertama dan kedua)
-const valencyValues = document.getElementById("valency-value");
-const kaKbValues = document.getElementById("ka-kb-value");
-const molalValues = document.getElementById("molal-value");
-const massValues = document.getElementById("mass-value");
-const volumeValues = document.getElementById("volume-value");
+const valencyValue = document.getElementById("valency-value");
+const kaKbValue = document.getElementById("ka-kb-value");
+const molalValue = document.getElementById("molal-value");
+const massValue = document.getElementById("mass-value");
+const volumeValue = document.getElementById("volume-value");
+const mrValue = document.getElementById("mr-value");
 
 /**
  * Selanjutnya kita simpan setiap jenis input;
@@ -22,7 +24,7 @@ const volumeValues = document.getElementById("volume-value");
  * dan memilih selection, page akan direload
  */
 
-const allInputs = [soluteType, inputType, valencyValues, molalValues, massValues, volumeValues];
+const allInputs = [soluteType, inputType, valencyValue, molalValue, massValue, volumeValue];
 allInputs.forEach((input) => {
     input.addEventListener('change', ()=> {
         updateUI();
@@ -47,6 +49,7 @@ function updateUI(){
     if (input === 'mass-and-volume'){
         containerMass.style.display = 'block';
         containerVolume.style.display = 'block';
+        containerMr.style.display = 'block';
 
         containerMolality.style.display = 'none';
     } else {
@@ -54,5 +57,85 @@ function updateUI(){
 
         containerMass.style.display = 'none';
         containerVolume.style.display = 'none';
+        containerMr.style.display = 'none';
+    }
+}
+
+/**
+ * Selanjutnya, kita membuat sebuah fungsi untuk menyimpan semua input
+ * Fungsi ini akan return sebuah objek, agar setiap propertinya bisa diakses
+ * oleh setiap helper function
+ */
+//Fungsi untuk mengambil semua input
+function getInputs (){
+    return {
+        type: soluteType.value,
+        method: inputType.value,
+        valency: parseInt(valencyValue.value) || 1,
+        molal: parseFloat(molalValue.value),
+        mass: parseFloat(massValue.value),
+        volume: parseFloat(massValue.value),
+        mr: parseFloat(mrValue.value),
+        kaKb: parseFloat(kaKbValue.value.replace(',','.'))
+    };
+}
+
+//Fungsi untuk menghitung konsentrasi/molal
+function getMolality (inputs){
+    if (inputs.method === 'molality'){
+        return inputs.concentration;
+    }
+    else {
+        if (inputs.mass > 0 && inputs.mr && inputs.volume > 0){
+            //Rumus molar = mass/Mr (massa molekul relatif)
+            const molar = mass/mr;
+
+            //Ubah volume dari mL ke L
+            const volumeLiter = volume/1000;
+
+            //Rumus molal = mol / Volume (L)
+            return molar/volumeLiter;
+        }
+    }
+    return NaN; //NaN kalau input tidak valid
+}
+
+//Fungsi untuk hitung pH dan pOH
+function calculatePh(type, molal, inputs){
+    let pH = null;
+    let pOH = null;
+
+    switch (type){
+        //Larutan bersifat kuat, menggunakan valensi
+        case 'asam-kuat':{
+            const hPlus = inputs.valency * molal;
+            pH = -Math.log10(hPlus);
+            break;
+        }
+        case 'basa-kuat':{
+            const ohMin = inputs.valency * molal;
+            pOH = -Math.log10(ohMin);
+            break;
+        }
+
+        //Larutan bersifat lemah, menggunakan Ka/Kb
+        case 'asam-lemah':{
+            //Cek apakah nilai Ka  kurang atau sama dengan nol atau berupa string asal
+            if (inputs.kaKb <= 0 || isNaN(inputs.kaKb)){
+                return null;
+            }
+            const hPlus = Math.sqrt(inputs.kaKb * molal);
+            pH = -Math.log10(hPlus);
+            break;
+        }
+        case 'basa-lemah': {
+                //Cek apakah nilai Kb  kurang atau sama dengan nol atau berupa string asal
+                if (inputs.kaKb <= 0 || isNaN(inputs.kaKb)){
+                    return null;
+                }
+                const ohMin = Math.sqrt(inputs.kaKb * molal);
+                pOH = -Math.log10(ohMin);
+                break;
+        }
     }
 }
