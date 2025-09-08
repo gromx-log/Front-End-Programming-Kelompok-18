8201fe781a87836128b07589bbaa3365ccaf6e89
gromx-log <filbertferdinand270@gmail.com>
2025-09-08 19:55:05 +0700

    Finished all the logics, for the calculation to work

--- Tugas_1/index.js
+++ Tugas_1/index.js
@@ -1,8 +1,8 @@
 //Referensi pertama kita, didasarkan pada section pertama
 const soluteType = document.getElementById("solute-type");
-const inputType = document.getElementById("input-type")
+const inputType = document.getElementById("input-type");
 
-//Untuk memunculkan input yang diperlukan (untuk section kedua)
+//Untuk memunculkan container input yang diperlukan (untuk section kedua)
 const containerValency = document.getElementById("container-valency");
 const containerKaKb = document.getElementById("container-ka-kb");
 const containerMolality = document.getElementById("container-molality");
@@ -18,6 +18,8 @@ const massValue = document.getElementById("mass-value");
 const volumeValue = document.getElementById("volume-value");
 const mrValue = document.getElementById("mr-value");
 
+//Memunculkan hasil perhitungan akhir
+const containerResult = document.getElementById("container-result");
 /**
  * Selanjutnya kita simpan setiap jenis input;
  * Tujuannya adalah agar setiap kali user mengetik angka atau teks
@@ -28,7 +30,11 @@ const allInputs = [soluteType, inputType, valencyValue, molalValue, massValue, v
 allInputs.forEach((input) => {
     input.addEventListener('change', ()=> {
         updateUI();
+        calculateAllAndDisplay();
     })
+    if (input.type === 'number' || input.type === 'text'){
+        input.addEventListener('input', calculateAllAndDisplay);
+    }
 })
 
 //Perubahan UI (Berpengaruh di Section 2)
@@ -74,7 +80,7 @@ function getInputs (){
         valency: parseInt(valencyValue.value) || 1,
         molal: parseFloat(molalValue.value),
         mass: parseFloat(massValue.value),
-        volume: parseFloat(massValue.value),
+        volume: parseFloat(volumeValue.value),
         mr: parseFloat(mrValue.value),
         kaKb: parseFloat(kaKbValue.value.replace(',','.'))
     };
@@ -83,15 +89,15 @@ function getInputs (){
 //Fungsi untuk menghitung konsentrasi/molal
 function getMolality (inputs){
     if (inputs.method === 'molality'){
-        return inputs.concentration;
+        return inputs.molal;
     }
     else {
         if (inputs.mass > 0 && inputs.mr && inputs.volume > 0){
             //Rumus molar = mass/Mr (massa molekul relatif)
-            const molar = mass/mr;
+            const molar = inputs.mass/inputs.mr;
 
             //Ubah volume dari mL ke L
-            const volumeLiter = volume/1000;
+            const volumeLiter = inputs.volume/1000;
 
             //Rumus molal = mol / Volume (L)
             return molar/volumeLiter;
@@ -137,5 +143,82 @@ function calculatePh(type, molal, inputs){
                 pOH = -Math.log10(ohMin);
                 break;
         }
+    }
+
+    //Isi nilai pH dan pOH untuk ditampilkan nanti
+    if (pOH !== null && pH === null){
+        pH = 14 - pOH;
+    }
+    else if(pOH === null && pH !== null){
+        pOH = 14 - pH;
+    }
+
+    //Return object berupa pH dan pOH
+    return {
+        pH, pOH
+    }
+}
+
+//Tampilkan analisa / sifat dari pH
+//Deskripsi diambil berdasarkan website: https://www.zenius.net/blog/cara-menghitung-ph-larutan-asam-basa/
+function getpHDescription(pH){
+    if (pH < 0 || pH > 14)  return "Tidak Terdefinisi";
+    if (pH > 11)            return "Sangat Basa";
+    if (pH > 9.5)           return "Basa";
+    if (pH > 7.5)           return "Sedikit Basa";
+    if (pH > 6.5)           return "Netral";
+    if (pH > 4.5)           return "Sedikit Asam";
+    if (pH > 3)             return "Asam";
+    if (pH >= 0)            return "Sangat Asam";
+}
+//Menampilkan hasil perhitungan pH dan pOH
+function displayResult(pH, pOH){
+    if (pH === null || isNaN (pH) || !isFinite(pH)){
+        containerResult.innerHTML = '<p>Hasil tidak dapat dihitung</p>';
+        containerResult.classList.add ('result-placeholder');
+        return
+    }
+    const description = getpHDescription(pH);
+    //Hapus placeholder kita
+    containerResult.classList.remove('result-placeholder');
+
+    containerResult.innerHTML = `
+        <div class="ph-value">${pH.toFixed(2)}</div>
+        <div class="poh-value">${pOH.toFixed(2)}</div>
+        <div class="ph-description">${description}</div>
+    `
+}
+
+//Fungsi utama untuk kalkulasi program ini
+function calculateAllAndDisplay(){
+    try {
+        //Ambil semua input data yang diberikan
+        const inputs = getInputs();
+
+        //Lalu cari nilai Molal/konsentrasinya
+        const molal = getMolality(inputs);
+
+        if (isNaN(molal) || molal <= 0){
+            containerResult.innerHTML = `<p>Hasil akan muncul di sini</p>`;
+            containerResult.classList.add('result-placeholder');
+            return;
+        }
+
+        const results = calculatePh (inputs.type, molal, inputs);
+
+        if(results && results.pH !== null){
+            displayResult(results.pH, results.pOH);
+        }
+        else {
+            let message = 'Hasil akan muncul disini';
+            if ((inputs.type === 'asam-lemah' || inputs.type === 'basa-lemah') && isNaN(inputs.kaKb)){
+                message = `Masukkan nilai Ka / Kb yang valid`;
+            }
+            containerResult.innerHTML = `<p>${message}</p>`
+            containerResult.classList.add(`result-placeholder`);
+        }
+    } catch (error){
+        containerResult.innerHTML = `<p style="color: red;">Error: INPUT TIDAK VALID!</p>`
+        containerResult.classList.add('result-placeholder');
     }
 }
